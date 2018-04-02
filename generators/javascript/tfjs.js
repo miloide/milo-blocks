816e20b76eb84781affd604e510683cb316d2710
Arjun Rao <mailarjunrao@gmail.com>
2018-04-03 02:39:23 +0530

    FIX: TF Blocks

--- generators/javascript/tfjs.js
+++ generators/javascript/tfjs.js
@@ -69,6 +69,6 @@ Blockly.JavaScript['tf_arithmetic'] = function(block) {
 
 Blockly.JavaScript['tf_print'] = function(block) {
     var value_tensor = Blockly.JavaScript.valueToCode(block, 'tensor', Blockly.JavaScript.ORDER_ATOMIC);
-    var code = 'console.webLog(' + value_tensor +'.toString().split("values:")[1].trim());\n';
+    var code = 'console.webLog(' + value_tensor +'.toString().split("values:")[1]);\n';
     return code;
   };
